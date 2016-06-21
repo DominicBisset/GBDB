8f497bb83888b9f81788edb391a2532405b432ef
Dominic Bisset <mail@dominicbisset.co.uk>
2016-06-21 22:19:33 +0100

    Set up initial project structure

---
+++ Scripts/typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="bluebird/bluebird.d.ts" />
+/// <reference path="bookshelf/bookshelf.d.ts" />
+/// <reference path="create-error/create-error.d.ts" />
+/// <reference path="knex/knex.d.ts" />
+/// <reference path="lodash/lodash-3.10.d.ts" />
+/// <reference path="node/node.d.ts" />
